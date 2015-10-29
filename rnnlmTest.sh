d37231bb247caf63473b567464d1c084a006b991
Man of Wax <undicizeri@gmail.com>
2015-10-29 22:56:05 +0100

    Added some comments, added perl regex to remove shit

--- rnnlmTest.sh
+++ rnnlmTest.sh
@@ -1,16 +1,23 @@
-corpus=all.noURL.txt
+corpus=$1
 posNegDir=PosNeg
 mkdir $posNegDir
 
-awk '{if(NF>4) print $0}' $corpus > $corpus.4.txt
+#take only lines with at least 4 tokens
+awk '{if(NF>4) print tolower($0);}' $corpus > $corpus.4.txt
 corpus=$corpus.4.txt
 
+#split the corpus in positive and negative sentences using emoticons
 grep -E -i "kappa|4head|kreygasm|elegiggle" $corpus > $posNegDir/all.pos
 grep -E -i "wutface|notlikethis|failfish|biblethump|dansgame|babyrage" $corpus > $posNegDir/all.neg
 
 cd $posNegDir
-sed -e "s/kappa//gI;s/4head//gI;s/kreygasm//gI;s/elegiggle//gI" < all.pos > all_pos_processed.txt
-sed -e "s/wutface//gI;s/notlikethis//gI;s/failfish//gI;s/biblethump//gI;s/dansgame//gI;s/babyrage//gI" < all.neg > all_neg_processed.txt
+#remove the emoticons
+sed -e "s/kappa//gI;s/4head//gI;s/kreygasm//gI;s/elegiggle//gI" < all.pos > all.pos.tmp
+sed -e "s/wutface//gI;s/notlikethis//gI;s/failfish//gI;s/biblethump//gI;s/dansgame//gI;s/babyrage//gI" < all.neg > all.neg.tmp
+#remove double or multiple spaces
+sed -e "s/  \+/ /g" < all.pos.tmp > all_pos_processed.txt
+sed -e "s/  \+/ /g" < all.neg.tmp > all_neg_processed.txt
+
 shuf -n 12500 all_neg_processed.txt > train-neg.txt
 shuf -n 12500 all_pos_processed.txt > train-pos.txt
 shuf -n 25000 all_pos_processed.txt > test-pos.txt
@@ -25,6 +32,7 @@ tail train-neg.txt -n 200 > valid
 rnnlm -rnnlm model-neg -train train -valid valid -hidden 50 -direct-order 3 -direct 200 -class 100 -debug 2 -bptt 4 -bptt-block 10 -binary
 
 cat test-pos.txt test-neg.txt > test.txt
+#I don't know why but rnnlm needs the corpus with lines number
 awk 'BEGIN{a=0;}{print a " " $0; a++;}' < test.txt > test-id.txt
 rnnlm -rnnlm model-pos -test test-id.txt -debug 0 -nbest > model-pos-score
 rnnlm -rnnlm model-neg -test test-id.txt -debug 0 -nbest > model-neg-score
@@ -34,8 +42,8 @@ cd ..
 cat RNNLM-SCORE | awk ' \
 BEGIN{cn=0; corr=0;} \
 { \
-  if ($3<1) if (cn<50000) corr++; \
-  if ($3>1) if (cn>=50000) corr++; \
+  if ($3<1) if (cn<25000) corr++; \
+  if ($3>1) if (cn>=25000) corr++; \
   cn++; \
 } \
 END{print "RNNLM accuracy: " corr/cn*100 "%";}'
--- twokenizer/postTwokenizer.sh
+++ twokenizer/postTwokenizer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed 's!http[s]\?://\S*!URL!g' $1 > $1.noURL.txt
+#replece urls with the string URL and with the perl magic transform "K a p p a B o y s" in "KappaBoys"
+sed 's!http[s]\?://\S*!URL!g' $1 |  perl -ne 's/(?<=(?<!\pL)\pL) (?=\pL(?!\pL))//g; print;' > $1.processed.txt
