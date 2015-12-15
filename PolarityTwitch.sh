d37c3c4a5b5f31b9612a458c1033ed2c1c396545
Man of Wax <undicizeri@gmail.com>
2015-12-15 23:01:37 +0100

    fixed algoritm

--- PolarityTwitch.sh
+++ PolarityTwitch.sh
@@ -147,18 +147,18 @@ for i in "${arrayPos[@]}"
 do
     for y in "${arrayNeg[@]}"
     do
-        paste $MULTIRNNLMSCOREDIR/$i.score $MULTIRNNLMSCOREDIR/$y.score | awk '{print $1/$2;}' > $1-$2-SCORE
+        paste $MULTIRNNLMSCOREDIR/$i.score $MULTIRNNLMSCOREDIR/$y.score | awk '{print $1/$2;}' > $MULTIRNNLMSCOREDIR/$1.$2.SCORE
     done
 done
 
-paste $MULTIRNNLMSCOREDIR/*-SCORE > RNNLM-SCORE
+paste $MULTIRNNLMSCOREDIR/*.SCORE > RNNLM-SCORE
 
 cat RNNLM-SCORE | awk '\
 BEGIN{cn=0; corr=0;} \
 { \
   tmp_pos=0;
   tmp_neg=0;
-  for(i=0;i<NF;i++) ($i<1) ? tmp_pos++ else tmp_neg++ \    
+  for(i=0;i<NF;i++) ($i<1) ? tmp_pos++ else tmp_neg++; \    
   if (tmp_pos<=tmp_neg) if (cn<size) corr++; \
   if (tmp_pos>tmp_neg) if (cn>=size) corr++; \
   cn++; \
